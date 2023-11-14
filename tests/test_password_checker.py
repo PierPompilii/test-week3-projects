96aa2e1a60be677d806b9556e8780ffc420f5fb6
Pier Bruno Pompilii <pier340@gmail.com>
2023-11-14 09:50:22 +0000

    testing commit success

--- tests/test_password_checker.py
+++ tests/test_password_checker.py
@@ -1,13 +1,14 @@
+import pytest
 from lib.password_checker import *
 
-def password_checker_is_true():
+def test_password_checker_is_true():
     passwordchecker = PasswordChecker()
     passwordchecker.check("abcdefgh")
     assert True
 
-def password_checker_not_True():
+def test_password_checker_not_True():
     passwordchecker = PasswordChecker()
     with pytest.raises(Exception) as e:
         passwordchecker.check ("abcde")
-    error_message = str(e.value)
-    assert error_message == "Invalid password, must be 8+ characters."
+        error_message = str(e.value)
+        assert error_message == "Invalid password, must be 8+ characters."
